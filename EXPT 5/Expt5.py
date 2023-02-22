d5a7fd50b50e97d40ac8c38ec013896d03aaa03c
Dharsan <dharsan19082002@gmail.com>
2023-02-22 10:11:53 +0530

    Feat: Expt5 added

---
+++ EXPT 5/Expt5.py
@@ -0,0 +1,9 @@
+import time
+import RPi.GPIO as GPIO       ## Import GPIO library
+GPIO.setmode(GPIO.BOARD)      ## Use board pin numbering
+GPIO.setup(18, GPIO.OUT)      ## Setup GPIO Pin 11 to OUT
+while True:
+	GPIO.output(18,True)  ## Turn on Led
+	time.sleep(1)         ## Wait for one second
+	GPIO.output(18,False) ## Turn off Led
+	time.sleep(1)         ## Wait for one second
